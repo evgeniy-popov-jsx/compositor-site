80937c8f193578fefaf4e5c678ddb362c7ba4083
unknown <evgeniy.popov.jsx@gmail.com>
2024-07-07 14:56:17 +0300

    del import

--- src/presentation/pages/Text/Text.tsx
+++ src/presentation/pages/Text/Text.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { ContentWrapper } from 'presentation/components/Content-wrapper/Content-wrapper';
-import { CompositionInfo } from 'presentation/components/Composition-info/Composition-info';
 import { data } from 'domain/mock/texts';
 import { NavigateBack } from 'presentation/components/Navigate-back/Navigate-back';
 import { Styled } from './styles';
