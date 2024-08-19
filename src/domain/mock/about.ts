b3db33ad30e9199588e7c00d529d6917578bb616
unknown <evgeniy.popov.jsx@gmail.com>
2024-08-19 22:17:14 +0300

    refactoring meta, corrected about

--- src/domain/mock/about.ts
+++ src/domain/mock/about.ts
@@ -3,10 +3,10 @@ export const data = {
   description: `
   we are a composer and a text manufacturer whose practice is situated between two medialities: discursive reflection, registered in texts, and practical experiments, taking place between written scores, live performances, and recorded tracks. following the testament of susan sontag, we emphasize art as an instrument of analysis — linking artistic exploration with theory.
   we usually start working on a piece with reflections on theoretical abstractions, consisting of culturological, philosophical, social and semiotic problematics. afterwards, a long process of searching and developing the adequate compositional methods and techniques for the defined theoretical concept follows. that’s the reason why our creating procedure always lasts a while, as for each piece we are trying to re-assemble our relations to writing, ourselves, and the world.
-  the research topics we consider significant for ourselves are personal and collective memories, performativity, power structures, medial conditionality, physical and virtual spaces, identity, strategies of resistance, rituals, and affects. we enjoy working with various mediums, devices, and self-made instruments, such as transducers, turntables, automated arciorgano, loudspeakers, needles, etc.
+  the research topics we consider significant for ourselves are personal and collective memories, performativity, power structures, medial conditionality, physical and virtual spaces, identity, strategies of resistance, rituals and affects. we enjoy working with various mediums, devices, and self-made instruments, such as transducers, turntables, automated arciorgano, loudspeakers, needles, etc.
   since the beginning of 2023, we have been giving seminars on the intersection between composition and media theory, as well as individual lessons at stuttgart university of music and arts. it’s a big pleasure for us to prepare lectures and colloquiums, as we see it as a significant part of our artistic activities. we enjoy being regularly invited as a speaker to workshops, symposiums and seminars.
   we refuse to be categorized into any social strata or to define ourselves with any nationality. please avoid this in naming us if you can.
-  — Polina
+  — polina
   `,
   cv: 'https://drive.google.com/file/d/1SWSODBR6ww7lLq9pD15zd-uKu1GxK7Ve/view?usp=drive_link',
 }
